f34846bee066c6f6b6dde78e13b6a6fe77892d1e
mang117 <idv17c.jjimenez@uartesdigitales.edu.mx>
2017-09-21 20:09:57 -0500

    Modificado por: "Fernando"
      Versiòn: 1.3
    
      En esta versión se modifica lo siguiente:
        1. Se Modifica el menu
        2. Se agregan la funcion (9) de decima a Binario
    
      Fuente: No

--- Math_Lib/Math_Lib/Math_Lib.py
+++ Math_Lib/Math_Lib/Math_Lib.py
@@ -25,11 +25,17 @@ print "- (7) Binario a Hexadecimal"
 print "- (8) Decimal a Hexadecimal"
 print "- (9) Decimal a Binario"
 
+print "- (10) Hexadecimal a Binario"
+print "- (11) Hexadecimal a Decimal"
+print "- (12) Binario a Decimal"
+print "- (13) Comprobacion de numeros primos"
+print "- (14) Numeros primos por rango"
+
 print ""
 
 print "Las funciones que puede realizar en su persona son las siguientes: "
 print ""
-print "- (10) Indice de Masa Corporal (IMC)"
+print "- (15) Indice de Masa Corporal (IMC)"
 print "-"*50
 
 n=int (raw_input("Que es lo que quieres realizar: "))
@@ -43,30 +49,45 @@ def multiplicacion (a):
     return a
 def division (a,b):
    return float(a/b)
-def modulo (a,b,):
+def modulo (a,b):
     return a%b 
 def exponente (a,b):  #Falta que se pueda ingresar decimal
     return float(a**b)
 def raiz (a,b):
     return math.sqrt(a,b)
+def BaH (a):
+    return a 
+def DaH (a):
+    return a
+def DaB (a):
+    return a 
+def HaB (a):
+    return a 
+def HaD (a):
+    return a
+def BaD (a):
+    return a 
+def primos (a,b):
+    return (a,b)
+def primosrang (a,b):
+    return (a,b)
 
 
 if (n==1) : #Suma
     print "Suma"
     print ""
-    a=int(raw_input("Escriba cantidad de numeros a a sumar: "))
+    a=int(raw_input("Escriba cantidad de numeros a sumar: "))
     i=0
     r=0
     while(i<a):
         b=int(raw_input("Numero: "))
         r=r+b
         i=i+1
-#Hola
     print "El resultado de la suma es: " +str(suma(r))
 elif (n==2): #Multiplicacion
     print "Multiplicacion"
     print ""
-    a=int(raw_input("Escriba cantidad de numeros a a multiplicar: "))
+    a=int(raw_input("Escriba cantidad de numeros a multiplicar: "))
     i=0
     r=1
     while(i<a):
@@ -102,6 +123,26 @@ elif (n==6): #Raiz
         print"Si el valor es menor que 2, el indice se toma al cuadrado por defecto"
         a=2
     print "Su resultado es: " +str(math.sqrt(b))
+elif (n==7): #Binario a Hexadecimal --Falta completar
+    print "Binario a Hexadecimal" 
+    print "" 
+elif (n==8): #Decimal a Hexadecimal -- Falta completar
+    print "Decimal a Hexadecimal"
+    print ""
+elif (n==9): #Decimal a Binario
+    print "Decimal a Binario"
+    print ""
+    numero=int(raw_input("Ingrese un numero: "))
+    a=[]
+    while(numero>0):
+        if(numero%2==0):
+            a.append(0)
+        else:
+            a.append(1)
+        numero=numero/2
+    a.reverse()
+    print "Su resultado es: " +  str(DaB(a))
+
 
 else:
     print "No existe ese valor"
